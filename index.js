e5272a6cc7924166b905bb30a7a3bf8833d096a7
lhleung <grandq33769@gmail.com>
2020-08-20 22:28:49 +0800

    fix(selector): Update selector position

--- index.js
+++ index.js
@@ -44,12 +44,11 @@ const writefile = (text) => {
   });
   const signInSelector = '.row-fluid:nth-child(3) button:nth-child(1)';
   const signOutSelector = '.row-fluid:nth-child(3) button:nth-child(2)';
-  const signInSystemSelector = '.row-fluid:nth-child(6) button';
-  const lookupSelector = '.row-fluid:nth-child(5) button';
+  const lookupSelector = '.row-fluid:nth-child(4) button:nth-child(1)';
   // 學校日曆
   const calendarUrl = 'https://eadm.ncku.edu.tw/welldoc/iftwf/WF8F11A.php?f_menuname=%AD%D3%A4H%AEt%B0%B2%A4%EB%BE%E4%AA%ED'
   // 登入行政 e 化系統
-  const eSystemSelector = '.row-fluid:nth-child(6) button';
+  const eSystemSelector = '.row-fluid:nth-child(5) button';
   const action = program.action;
 
   // 打卡 or 登入
